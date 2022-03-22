1bfc1e4188cd958713f8a221c9e7e87e45ada286
SexyBeast42 <davakoeswoyo11@gmail.com>
2022-03-22 21:51:08 +0100

    Player Attack done + cooldown limit of 3 on the players roll.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public Transform player;
+    
     public float moveSpeed = 1.5f;
     private Rigidbody2D rb;
     private Vector2 movement;
+    
     public HealthBarEnemy Healthbar;
     public float Hitpoints;
     public float MaxHitpoints = 5f;
+    
+    //List of enemies
+    private static List<Enemy> enemyList;
 
-        
     void Start()
     {
         Hitpoints = MaxHitpoints;
@@ -22,24 +26,46 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
+        FindTarget();
+    }
+    
+    private void FixedUpdate(){
+        MoveCharacter(movement);
+    }
+    
+    void MoveCharacter(Vector2 direction){
+        rb.MovePosition ((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+    
+    public void TakeDamage(float damage)
+    {
+        //Debug.Log(gameObject.name + " got damaged.");
+        Hitpoints -= damage;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
+
+        StartCoroutine(DazedTime());
+        
+        if(Hitpoints <= 0){
+            //Debug.Log(gameObject.name + " dieded.");
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator DazedTime()
+    {
+        moveSpeed = 0f;
+        yield return new WaitForSeconds(.6f);
+        moveSpeed = 1.5f;
+    }
+
+    private void FindTarget()
+    {
         Vector3 direction = player.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         rb.rotation = angle;
         direction.Normalize();
         movement = direction;
-
     }
-    private void FixedUpdate(){
-        moveCharacter(movement);
-    }
-    void moveCharacter(Vector2 direction){
-        rb.MovePosition ((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
-    }
-    public void TakeHit(float damage){
-        Hitpoints -= damage;
-        Healthbar.SetHealth(Hitpoints,MaxHitpoints);
-        if(Hitpoints <= 0){
-            Destroy(gameObject);
-        }
-    }
+    
+    
 }
