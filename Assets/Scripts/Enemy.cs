f8c0c510fe72f41f6c4f806f4c738b02c63ecef7
SexyBeast42 <davakoeswoyo11@gmail.com>
2022-05-02 18:16:51 +0200

    Enemy Detection in progress

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,20 +12,30 @@ public class Enemy : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movement;
     
+    //Enemy's Vision
+    public Transform castPoint;
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+    public float viewRadius;
+    [Range(0, 360)] public float viewAngle;
+
     //Enemy's health
     public HealthBarEnemy Healthbar;
     public float Hitpoints;
     public float MaxHitpoints = 5f;
 
+    //Enemy's attack
     private float distToPlayer;
+    public Transform FirePoint;
     
-    public enum EnemyType
+    //Enemy action
+    private enum State
     {
-        Melee,  
-        Ranged,
+        idle,
+        attacking,
     }
-
-    public EnemyType enemyType;
+    
+    private State state;
 
     void Awake()
     {
@@ -36,34 +47,21 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         // FindTarget();
+        
+        // distToPlayer = 2f;
 
-        switch (enemyType)
-        {
-            case EnemyType.Melee:
-                distToPlayer = 2f;
-
-                MoveToTarget(distToPlayer);
-                // Melee
-                // Move towards player in a certain distance
-                // if reached desired pos, attack player
-                
-                break;
-            
-            case EnemyType.Ranged:
-                break;
-        }
+        // MoveCharacter(EnemyDetection());
+        // Melee
+        // while enemy not in range, move towards player in a certain distance
+        // if reached desired pos, attack player
     }
     
     private void FixedUpdate()
     {
+        EnemyDetection();
         MoveCharacter(movement);
     }
-    
-    void MoveCharacter(Vector2 direction)
-    {
-        rb.MovePosition ((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
-    }
-    
+
     public void TakeDamage(float damage)
     {
         // Debug.Log(gameObject.name + " got damaged.");
@@ -85,18 +83,65 @@ public class Enemy : MonoBehaviour
         moveSpeed = 1.5f;
     }
 
-    private void MoveToTarget(float distance)
+    void MoveCharacter(Vector2 direction)
     {
-        // raycast infront, check if the line is the distance we want
-        // move to still be in set line
-        // later in update logic, if raycast line is in correct pos, then do attack
+        rb.MovePosition ((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+    private void EnemyDetection()
+    {
+        print("work");
+        bool canSeeTarget;
+        Vector3 playerLocation;
+
+        Collider2D[] targetsInViewRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+
+            Debug.Log("Can see player2");
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float distToTarget = Vector3.Distance(transform.position, target.position);
+
+                Debug.Log("Can see player3");
+                if (!Physics.Raycast(transform.position, dirToTarget, distToTarget, obstacleMask))
+                {
+                    Debug.Log("Can see player");
+                }
+            }
+        }
         
+        //return playerLocation;
+    }
+
+    private Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, viewRadius);
+    }
+
+    /*private void MoveToTarget(float distance)
+    {        
         Vector3 direction = player.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         
         rb.rotation = angle;
         movement = direction.normalized;
-    }
+    }*/
+    
     
     /// Enemy has 8 raycast rays in all directions / or use navmeshes (either way - do later aft basics established)
     /// They are smart enough to hide behind walls
