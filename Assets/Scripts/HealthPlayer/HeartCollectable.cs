66a028fd38fc1c0d57a3e88a2ed993fcb5a06f15
Arktik779 <arktik779@gmail.com>
2022-06-20 17:43:51 +0300

    Added UI, menu added
    Added UI, menu added

--- Assets/Scripts/HealthPlayer/HeartCollectable.cs
+++ Assets/Scripts/HealthPlayer/HeartCollectable.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class HeartCollectable : MonoBehaviour
 {
-[SerializeField] private float healthValue;
-public GameObject E;
+    [SerializeField] private float healthValue;
+    public GameObject E;
+    bool playerInCollider = false;
+    Collider2D playerCollider = null;
 
 
-private void OnTriggerEnter2D(Collider2D collision){
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.tag == "Player")
+        {
+            E.SetActive(true);
+            playerInCollider = true;
+            playerCollider = collision;
+        }
+    }  
 
-    if (collision.tag == "Player")
+    void Update()
     {
-   E.SetActive(true); 
-   collision.GetComponent<Health>().AddHealth(healthValue);
+        if (playerInCollider && Input.GetKeyDown(KeyCode.E)) {   
+            playerCollider.GetComponent<Health>().AddHealth(healthValue);
+        }
     }
- 
-}
-     
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Player"){   
+            E.SetActive(false);
+            playerInCollider = false;
+            playerCollider = null;
+        }
+    }
 }
 
 
